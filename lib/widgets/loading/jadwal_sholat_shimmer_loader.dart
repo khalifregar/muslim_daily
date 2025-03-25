e8c25000b54984f891cd6e618cb71ecef94e3451
khalifsiregar123 <khalifsiregar123@gmail.com>
2025-03-26 02:41:51 +0700

    update Ui carousel, refresh

--- lib/widgets/loading/jadwal_sholat_shimmer_loader.dart
+++ lib/widgets/loading/jadwal_sholat_shimmer_loader.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shimmer/shimmer.dart';
 
 class JadwalSholatShimmerLoader extends StatelessWidget {
@@ -7,47 +6,59 @@ class JadwalSholatShimmerLoader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
-      child: Padding(
-        padding: EdgeInsets.all(16.w),
-        child: Shimmer.fromColors(
-          baseColor: Colors.grey.shade300,
-          highlightColor: Colors.grey.shade100,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 20.h,
-                width: 150.w,
-                color: Colors.white,
-              ),
-              SizedBox(height: 16.h),
-              ...List.generate(5, (index) => _buildLine()),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildLine() {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 8.h),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: 16.h,
-            width: 80.w,
-            color: Colors.white,
-          ),
-          Container(
-            height: 16.h,
-            width: 60.w,
-            color: Colors.white,
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: Colors.grey.shade200, width: 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.06),
+            blurRadius: 12,
+            offset: const Offset(0, 6),
           ),
         ],
+      ),
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey.shade300,
+        highlightColor: Colors.grey.shade100,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header shimmer
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(width: 120, height: 14, color: Colors.white),
+                Container(width: 80, height: 14, color: Colors.white),
+              ],
+            ),
+            const SizedBox(height: 20),
+
+            // 5 shimmer tiles
+            ...List.generate(5, (index) {
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(width: 80, height: 14, color: Colors.white),
+                        Container(width: 50, height: 14, color: Colors.white),
+                      ],
+                    ),
+                  ),
+                  if (index < 4)
+                    const Divider(height: 1, thickness: 1, color: Color(0xFFE0E0E0)),
+                ],
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
